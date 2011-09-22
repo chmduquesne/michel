658295d4de467bc71f3dc6dc7e66b210e97ddb6f
Christophe-Marie Duquesne <chm.duquesne@gmail.com>
2011-09-22 14:24:13 +0200

    adding __init__.py and license file.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='chm.duquesne@gmail.com',
     packages=find_packages(),
     url='https://github.com/chmduquesne/michel',
-    license='LICENSE.txt',
+    license=open('LICENSE.txt').read(),
     description='pushes/pull flat text files to google tasks',
     long_description=open('README.md').read(),
     install_requires = ['google-api-python-client'],
