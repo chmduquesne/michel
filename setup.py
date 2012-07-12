8da14b52f70185a7a43ca8b3292b16084d27e505
Christophe-Marie Duquesne <chm.duquesne@gmail.com>
2012-07-12 09:28:16 +0200

    updating version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='michel',
-    version='0.0.6',
+    version='0.0.7',
     author='Christophe-Marie Duquesne',
     author_email='chm.duquesne@gmail.com',
     packages=find_packages(),
